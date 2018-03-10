d85270fb5c5fe913f2d7110483be8cf67823aa29
Angelique Lazarus <javathehut@users.noreply.github.com>
2018-03-10 13:38:13 -0500

    fixing navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Navbar, NavItem, Nav, MenuItem, NavDropdown } from 'react-bootstrap';
+// import ResponsiveMenu from 'react-responsive-navbar';
 import { Link } from 'react-router';
 import logo from './images/angelLogo.png';
 
@@ -12,13 +13,10 @@ class App extends Component {
           <Navbar bsStyle="" /*This just takes away some of the default styling*/>
             <Navbar.Header className="brand">
               ANGELIQUE <Link to="/"><img className="logo" src={logo} alt="Angelique Lazarus logo" /></Link> LAZARUS
-            <Navbar.Toggle  className="toggler" /*This makes sure there's a hamburger on mobile*//>
-            
             </Navbar.Header>
-            <Navbar.Collapse>
               <Nav pullRight className="customNav">
                 <NavItem eventKey={1}><Link to="/">Portfolio</Link></NavItem>
-                <NavItem eventKey={2} href="https://drive.google.com/open?id=0B3KezJtvS9TAemNqTV9tcWJmVm8" target="_blank">Résumé</NavItem>
+                <NavItem eventKey={2} href="https://drive.google.com/open?id=0B3KezJtvS9TAemNqTV9tcWJmVm8" target="_blank">Download Résumé</NavItem>
                 <NavDropdown eventKey={3} title="Blogs" id="basic-nav-dropdown">
                   <MenuItem eventKey={3.1} href="http://chocolatycoding.com" target="_blank">Chocolaty Coding</MenuItem>
                   <MenuItem divider />
@@ -28,7 +26,7 @@ class App extends Component {
                 </NavDropdown>
                 <NavItem eventKey={5}><Link to="about">About</Link></NavItem>
               </Nav>
-            </Navbar.Collapse>
+            
           </Navbar>
         </div>
         <div className="container">
