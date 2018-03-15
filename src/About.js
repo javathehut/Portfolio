a1196854c43070c3bc374f6c8c4a3ccb735468bf
Angelique Lazarus <javathehut@users.noreply.github.com>
2018-03-14 20:07:11 -0400

    Updated info and fixed portfolio (from react-bootstrap to reactstrap)

--- src/About.js
+++ src/About.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import angel from './images/angel.jpg';
+import angel from './images/angelique1.jpg';
 import { Row, Col, Glyphicon } from 'react-bootstrap';
 import insta from './icons/instagram.png';
 import linkedin from './icons/linkedin.png';
@@ -13,12 +13,12 @@ class About extends Component {
     return (
       <div>
         <Row>
-          <Col xs={12} md={7}>
+          <Col xs={12} md={6}>
             <img className="aboutPic" src={angel} alt="Angelique Lazarus" />
           </Col>
-          <Col xs={12} md={5}>
+          <Col xs={12} md={6}>
             <div className="blurb">
-              WEB DEVELOPER SEEKING EXCITING CAREER/PROJECT OPPORTUNITIES
+              WEB DEVELOPER AND EDUCATOR ALWAYS SEEKING REWARDING OPPORTUNITIES
           </div>
           </Col>
         </Row>
@@ -28,21 +28,35 @@ class About extends Component {
         <div>
           <h2 className="lead aboutLead">
             Angelique is a web developer, educator, and artist. In essence, she is a creator. Her passion and enthusiasm for learning, problem-solving, building, and languages have led her to pursue these 3 rewarding endeavours that allow her to fully explores these areas of interest.
-          </h2>
+          </h2><br />
+          <p className="whatsUp">
+            <span className="aboutHeading">What she's up to currently:</span> <br />
+            Angelique is excited to begin her next opportunity in March 2018 as an Assistant 
+            Instructor at the University of Toronto's Web Development bootcamp. By day, Angelique
+            teaches ESL in downtown Toronto.
+          </p>
           <hr />
           <p className="aboutMe">
-            <span className="aboutHeading">WEB DEVELOPMENT: </span>
-            An intrinsically motivated self-starter, Angelique has been coding since September 2015. She began on her own through various online resources (Free Code Camp, Code Academy, Udemy, etc.) and then decided to take it more seriously with BrainStation Toronto's full-time Web Development certificate program. She is itching to get into more opportunities to build good code and make people's lives easier. Angelique loves working in JavaScript and gets a kick out of building single page applications (SPAs) in React.
+            <span className="aboutHeading">WEB DEVELOPMENT</span> <br />
+            An intrinsically motivated self-starter, Angelique has been coding since September 2015. 
+            She began on her own through various online resources (Free Code Camp, Code Academy, Udemy, etc.) 
+            and then decided to take it more seriously with BrainStation Toronto's full-time Web Development 
+            certificate program. She loves working in JavaScript and gets a kick out of building single 
+            page applications (SPAs) in React.
           </p>
           <p className="aboutMe">
-            <span className="aboutHeading">EDUCATION: </span>
-            Angelique has 8 years of experience teaching ESL (in Canada and South Korea) and an M.Ed. degree in Adult Education and Community Development. As such, she is adaptable, flexible, and one hell of a communicator.
+            <span className="aboutHeading">EDUCATION</span><br />
+            Angelique has 8 years of experience teaching ESL (in Canada and South Korea) and an M.Ed. degree in 
+            Adult Education and Community Development. As such, she is adaptable, flexible, and one hell of a 
+            communicator.
           </p>
           <p className="aboutMe">
-            <span className="aboutHeading">ART: </span>
-            That one person in the room singing something or other? That's Angelique.
-        Much of her art has been vocal, from choral singing, to solo performances, musical theatre, and <Link to="https://www.voices.com/people/angelique333">voice acting. </Link>
-            Check her out in episode 3 of <strong><Link to="http://bigdata.show" target="_blank">'Big Data' by Ryan Estrada.</Link></strong>
+            <span className="aboutHeading">ART</span><br />
+            That one person in the room always singing something or other? That's Angelique.
+            Much of her art has been vocal, from choral singing, to solo performances, musical theatre, 
+            and <Link to="https://www.voices.com/people/angelique333">voice acting. </Link>
+            Check her out in episode 3 of <strong><Link to="http://bigdata.show" target="_blank">'Big Data' 
+            by Ryan Estrada.</Link></strong>
           </p>
           <hr />
         </div>
--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Grid, Row, Thumbnail, Col, Glyphicon } from 'react-bootstrap';
-import {portfolioItems} from './portfolioItems.js';
+import { portfolioItems } from './portfolioItems.js';
 import insta from './icons/instagram.png';
 import linkedin from './icons/linkedin.png';
 import twitter from './icons/twitter.png';
 import github from './icons/github.png';
 import { Link } from 'react-router';
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, CardLink
+} from 'reactstrap';
 
 
 class Portfolio extends Component {
@@ -14,13 +18,18 @@ class Portfolio extends Component {
         let myPortfolio = portfolioItems.map((item, i) => {
             return (
                 <Col xs={12} md={4} key={i}>
-                <Thumbnail  className="pfLink pfpic" href={portfolioItems[i].link} target="_blank" src={portfolioItems[i].image} alt={portfolioItems[i].alt}>
-                    <h3 className="page-header">{portfolioItems[i].title}</h3>
-                    <p className="lead">{portfolioItems[i].description}</p>
-                    <p><strong>CREATED USING: </strong>{portfolioItems[i].tools}</p>
-                    <p><strong>CODED: </strong>{portfolioItems[i].team}</p>
-                </Thumbnail>
-            </Col>
+                    <CardLink href={portfolioItems[i].link} target="_blank">
+                        <Card className="pfLink">
+                            <CardImg className="pfpic" top width="100%" src={portfolioItems[i].image} alt={portfolioItems[i].alt} />
+                            <CardBody>
+                                <CardTitle className="page-header">{portfolioItems[i].title}</CardTitle>
+                                <CardSubtitle>{portfolioItems[i].description}</CardSubtitle>
+                                <CardText><p><strong>CREATED USING: </strong>{portfolioItems[i].tools}</p>
+                                    <p><strong>CODED: </strong>{portfolioItems[i].team}</p></CardText>
+                            </CardBody>
+                        </Card>
+                    </CardLink>
+                </Col>
             )
         })
 
@@ -35,15 +44,15 @@ class Portfolio extends Component {
                 </Grid>
                 <hr />
                 <div className="footer1">
-                <p><Glyphicon glyph="copyright-mark" /> Angelique Lazarus | <em>Site created using React & React-Bootstrap</em> </p>
-              </div>
-              <div className="footer2">
-                <Link to="https://www.github.com/javathehut" target="_blank"><img className="icons" src={github} alt="github" /></Link>
-                <Link to="https://www.linkedin.com/in/angeliquelazarus/" target="_blank"><img className="icons" src={linkedin} alt="linkedin" /></Link>
-      
-                <Link to="https://www.instagram.com/chocolatycoding" target="_blank"><img className="icons" src={insta} alt="instagram" /></Link>
-                <Link to="https://twitter.com/ChocolatyCoding" target="_blank"><img className="icons" src={twitter} alt="twitter" /></Link>
-              </div>
+                    <p><Glyphicon glyph="copyright-mark" /> Angelique Lazarus | <em>Site created using React & React-Bootstrap</em> </p>
+                </div>
+                <div className="footer2">
+                    <Link to="https://www.github.com/javathehut" target="_blank"><img className="icons" src={github} alt="github" /></Link>
+                    <Link to="https://www.linkedin.com/in/angeliquelazarus/" target="_blank"><img className="icons" src={linkedin} alt="linkedin" /></Link>
+
+                    <Link to="https://www.instagram.com/chocolatycoding" target="_blank"><img className="icons" src={insta} alt="instagram" /></Link>
+                    <Link to="https://twitter.com/ChocolatyCoding" target="_blank"><img className="icons" src={twitter} alt="twitter" /></Link>
+                </div>
             </div>
         );
     }
